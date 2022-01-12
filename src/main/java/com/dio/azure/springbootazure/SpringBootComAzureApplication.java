fc02cfa2d0b4b407f3d2bf96bc3ffc5ca216eb44
DevManah6Eugenio <manasses.dev@gmail.com>
2022-01-12 19:23:53 -0300

    primeiro commit

---
+++ src/main/java/com/dio/azure/springbootazure/SpringBootComAzureApplication.java
@@ -0,0 +1,13 @@
+package com.dio.azure.springbootazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootComAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootComAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/azure/springbootazure/SpringBootComAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.azure.springbootazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootComAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
